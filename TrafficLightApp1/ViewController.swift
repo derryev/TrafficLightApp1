6ccd82d6fb38720333fab181d3f1a7c60a6fda9a
derryev <derryev@ssfs.org>
2024-04-01 14:51:54 -0400

    Initial Commit

---
+++ TrafficLightApp1/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TrafficLightApp1
+//
+//  Created by Eva Derryberry on 4/1/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
