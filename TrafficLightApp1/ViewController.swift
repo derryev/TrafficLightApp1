7466f8575ee4d49e5215c25200ab1a5dea46728f
derryev <derryev@ssfs.org>
2024-04-03 08:52:51 -0400

    Completed traffic light app 1

--- TrafficLightApp1/ViewController.swift
+++ TrafficLightApp1/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var textField: UITextField!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
+            
+        }
         // Do any additional setup after loading the view.
+    @IBAction func unwindToRed(unwindSegue: UIStoryboardSegue) {
+
+    
     }
     
-
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        segue.destination.navigationItem.title = textField.text
+    }
+    
 }
 
